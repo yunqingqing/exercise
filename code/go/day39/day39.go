ffad7e8c939c49bcdaf25e58f06078225b853081
yunqingqing <zzwzfh@gmail.com>
2019-03-10 21:20:23 +0800

    day39 add new solution

--- code/go/day39/day39.go
+++ code/go/day39/day39.go
@@ -1,5 +1,7 @@
 package day39
 
+import "container/heap"
+
 func partition(data *[]int, start, end int) int {
 	pivot := start
 	index := pivot + 1
@@ -13,7 +15,8 @@ func partition(data *[]int, start, end int) int {
 	return index - 1
 }
 
-func GetLeastNumbers(array []int, length, k int) []int {
+// partition解法
+func GetLeastNumbers1(array []int, length, k int) []int {
 	if k > length || array == nil || k < 0 {
 		return nil
 	}
@@ -35,6 +38,55 @@ func GetLeastNumbers(array []int, length, k int) []int {
 
 	for i := 0; i < k; i++ {
 		output = append(output, array[i])
+	}
+	return output
+}
+
+
+// heap解法
+type IntHeap []int
+
+func (h IntHeap) Len() int { return len(h) }
+
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+
+func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func GetLeastNumbers2(array []int, length, k int) []int {
+	if k > length || array == nil || k < 0 {
+		return nil
+	}
+	
+	h := &IntHeap{}
+	// 初始化堆
+	heap.Init(h)
+	for _, item := range(array) {
+		if h.Len() == k {
+			max := heap.Pop(h)
+			if max.(int) > item {
+				heap.Push(h, item)
+			} else {
+				heap.Push(h, max)
+			}
+		} else {
+			heap.Push(h, item)
+		}
+	}
+	output := []int{}
+	for h.Len() != 0 {
+		output = append(output, heap.Pop(h).(int))
 	}
 	return output
 }
--- code/go/day39/day39_test.go
+++ code/go/day39/day39_test.go
@@ -7,20 +7,32 @@ import (
 
 func TestFunc1(t *testing.T) {
 	array := []int{4, 5, 1, 6, 2, 7, 3, 8}
-	fmt.Println(GetLeastNumbers(array, len(array), 4))
+	fmt.Println(GetLeastNumbers1(array, len(array), 4))
+	fmt.Println("=============")
+	fmt.Println(GetLeastNumbers2(array, len(array), 4))
+	fmt.Println("======fun1 end=======")
 }
 
 func TestFunc2(t *testing.T) {
 	array := []int{4, 5, 1, 6, 2, 7, 3, 8}
-	fmt.Println(GetLeastNumbers(array, len(array), len(array)))
+	fmt.Println(GetLeastNumbers1(array, len(array), len(array)))
+	fmt.Println("=============")
+	fmt.Println(GetLeastNumbers2(array, len(array), len(array)))
+	fmt.Println("=======fun2 end======")
 }
 
 func TestFunc3(t *testing.T) {
 	array := []int{4, 5, 1, 6, 2, 7, 3, 8}
-	fmt.Println(GetLeastNumbers(array, len(array), len(array)+1))
+	fmt.Println(GetLeastNumbers1(array, len(array), len(array)+1))
+	fmt.Println("=============")
+	fmt.Println(GetLeastNumbers2(array, len(array), len(array)+1))
+	fmt.Println("=======fun3 end======")
 }
 
 func TestFunc4(t *testing.T) {
 	array := []int{4, 5, 1, 6, 2, 7, 3, 8}
-	fmt.Println(GetLeastNumbers(array, len(array), 1))
+	fmt.Println(GetLeastNumbers1(array, len(array), 1))
+	fmt.Println("=============")
+	fmt.Println(GetLeastNumbers2(array, len(array), 1))
+	fmt.Println("======fun4 end=======")
 }
