f6270ab238b95efffc4202904feeeba6fa7033b6
yunqingqing <zzwzfh@gmail.com>
2019-03-27 23:50:57 +0800

    补充代码

--- code/go/day45/day45.go
+++ code/go/day45/day45.go
@@ -1,1 +1,43 @@
 package day45
+
+import "strconv"
+
+func GetTranslationCount(number int) int {
+	if number < 0 {
+		return 0
+	}
+	stringNumber := strconv.Itoa(number)
+	return getTranslationCount(stringNumber)
+}
+
+func getTranslationCount(strNumber string) int {
+	length := len(strNumber)
+	counts := make([]int, length)  // 用来存放计算过的情况的次数
+	count := 0
+
+	for i := length - 1; i >= 0; i-- {
+		count = 0
+		if i < (length - 1) {
+			count = counts[i + 1]
+		} else {
+			count = 1
+		}
+		if i < (length - 1) {
+			digit1, _ := strconv.Atoi(string([]rune(strNumber)[i]))
+			digit2, _ := strconv.Atoi(string([]rune(strNumber)[i+1]))
+			converted := digit1 * 10 + digit2
+			if converted >= 10 && converted <= 25 {
+				if i < (length - 2) {
+					count += counts[i + 2]
+				} else {
+					count += 1
+				}
+			}
+		}
+		counts[i] = count
+
+	}
+	count = counts[0]
+
+	return count
+}
--- code/go/day45/day45_test.go
+++ code/go/day45/day45_test.go
@@ -2,6 +2,74 @@ package day45
 
 import "testing"
 
-func TestFunc(t *testing.T) {
+func TestFunc1(t *testing.T) {
+	number := 10
+	expected := 2
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
 
+func TestFunc2(t *testing.T) {
+	number := 0
+	expected := 1
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc3(t *testing.T) {
+	number := 126
+	expected := 2
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc4(t *testing.T) {
+	number := 426
+	expected := 1
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc5(t *testing.T) {
+	number := 100
+	expected := 2
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc6(t *testing.T) {
+	number := 101
+	expected := 2
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc7(t *testing.T) {
+	number := 12258
+	expected := 5
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc8(t *testing.T) {
+	number := -100
+	expected := 0
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc9(t *testing.T) {
+	number := 125
+	expected := 3
+	if GetTranslationCount(number) != expected {
+		t.Errorf("not correct.")
+	}
 }
