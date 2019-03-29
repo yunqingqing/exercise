9d15b3f9ca1c11a3bd1e11878c84876c7c77ac12
yunqingqing <zzwzfh@gmail.com>
2019-03-29 23:50:32 +0800

    day46代码

--- code/go/day46/day46.go
+++ code/go/day46/day46.go
@@ -1,1 +1,39 @@
 package day46
+
+func GetMaxValue(values [][]int, rows, cols int) int {
+	if values == nil || rows <= 0 || cols <= 0 {
+		return 0
+	}
+
+	maxValues := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		maxValues[i] = make([]int, cols)
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			left := 0
+			up := 0
+			if i > 0 {
+				up = maxValues[i - 1][j]
+			}
+
+			if j > 0 {
+				left = maxValues[i][j - 1]
+			}
+
+			// f(i,j) = max(f(i-1, j), f(i,j-1)) + gift[i,j]
+			maxValues[i][j] = max(left, up) + values[i][j]
+		}
+	}
+	maxValue := maxValues[rows - 1][cols - 1]
+
+	return maxValue
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- code/go/day46/day46_test.go
+++ code/go/day46/day46_test.go
@@ -2,6 +2,65 @@ package day46
 
 import "testing"
 
-func TestFunc(t *testing.T) {
+func TestFunc1(t *testing.T) {
+	values := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	excepted := 29
+	
+	if GetMaxValue(values, 3, 3) != excepted {
+		t.Errorf("not correct.")
+	}
+}
 
+func TestFunc2(t *testing.T) {
+	values := [][]int{
+        { 1, 10, 3, 8 },
+        { 12, 2, 9, 6 },
+        { 5, 7, 4, 11 },
+        { 3, 7, 16, 5 },
+	}
+	excepted := 53
+	
+	if GetMaxValue(values, 4, 4) != excepted {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc3(t *testing.T) {
+	values := [][]int{
+		{ 1, 10, 3, 8 },
+	}
+	excepted := 22
+	
+	if GetMaxValue(values, 1, 4) != excepted {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc4(t *testing.T) {
+	values := [][]int{
+        { 1 },
+        { 12 },
+        { 5 },
+        { 3 },
+	}
+	excepted := 21
+	
+	if GetMaxValue(values, 4, 1) != excepted {
+		t.Errorf("not correct.")
+	}
+}
+
+func TestFunc5(t *testing.T) {
+	values := [][]int{
+        { 3 },
+	}
+	excepted := 3
+	
+	if GetMaxValue(values, 1, 1) != excepted {
+		t.Errorf("not correct.")
+	}
 }
