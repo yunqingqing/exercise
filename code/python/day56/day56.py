b0d83938f8a897ce5cb5eb3c5a2dc3c44503d5e1
yunqingqing <zzwzfh@gmail.com>
2019-08-10 14:04:38 +0800

    day56 买彩票最佳时机

--- code/python/day56/day56.py
+++ code/python/day56/day56.py
@@ -3,8 +3,7 @@ def maxProfit(prices):
     :type prices: List[int]
     :rtype: int
     """
-    dp = [[0, 0]] * len(prices)
-    
+    dp = [[0, 0] for i in prices] 
     for i, price in enumerate(prices):
         if i - 1 < 0:
             dp[i][0] = 0
@@ -13,9 +12,8 @@ def maxProfit(prices):
         dp[i][1] = max(dp[i-1][1], 0-price)
         dp[i][0] = max(dp[i-1][0], dp[i-1][1]+price)
                 
-
-    print(dp)
-    print(len(prices))
+        print(i)
+        print(dp)
     return dp[len(prices)-1][0]
 
-maxProfit([])
+print(maxProfit([7,1,5,3,6,4]))
